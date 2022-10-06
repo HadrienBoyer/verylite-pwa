d866cd800caedba4b11883cb2d4999b81eabaec6
HadrienBoyer <hadrienboyer@gmail.com>
2022-10-06 07:50:28 +0200

    add the notifications to PWA

--- public/js/status.js
+++ public/js/status.js
@@ -1,9 +1,24 @@
 document.addEventListener('DOMContentLoaded', init, false);
 
-function init() {
+function init()
+{
   if (!navigator.onLine) {
     const statusElem = document.querySelector('.page-status')
 
     statusElem.innerHTML = 'offline'
   }
+
+  // Notifications:
+  if ("Notification" in window)
+  {
+    console.log("The Notifications API is supported");
+
+      Notification.requestPermission().then(permission => {
+          if (permission === "granted") {
+              console.log("The user accepted the notification");
+              const notification = new Notification("Hello World!");  //  Send a notification
+          }
+      });
+  }
+
 }
