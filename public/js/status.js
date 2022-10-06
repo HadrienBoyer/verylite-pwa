9bb50b57a61540e1c8045dfcff636c3a6a9c36f6
HadrienBoyer <hadrienboyer@gmail.com>
2022-10-06 08:09:05 +0200

    add TODO for Notifications

--- public/js/status.js
+++ public/js/status.js
@@ -8,7 +8,9 @@ function init()
     statusElem.innerHTML = 'offline'
   }
 
-  // Notifications:
+
+  // Notifications: https://learn.microsoft.com/en-us/microsoft-edge/progressive-web-apps-chromium/how-to/notifications-badges
+  //TODO: create notifications.js and add permission fallback in the cache: https://developer.mozilla.org/fr/docs/Web/API/Notifications_API/Using_the_Notifications_API
   if ("Notification" in window)
   {
     console.log("The Notifications API is supported");
